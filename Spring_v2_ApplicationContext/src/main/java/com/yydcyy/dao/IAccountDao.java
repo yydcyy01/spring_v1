d1e25b67764aa60a4c498e3a76026428c8b822ba
yuyang <447395536@qq.com>
2019-09-23 23:15:50 +0800

    添加 Spring_v2 :  Spring ApplicationContext 下 3 个常用类 :ClassPathXMlapplicationContext FileSystemXMLApplicationContext AnnotationConfigApplicatonContext

---
+++ Spring_v2_ApplicationContext/src/main/java/com/yydcyy/dao/IAccountDao.java
@@ -0,0 +1,13 @@
+package com.yydcyy.dao;
+
+/**
+ * @author YYDCYY
+ * @create 2019-09-23
+ *  * 账户的持久层接口
+ */
+public interface IAccountDao {
+    /**
+     * 模拟保存账户
+     */
+    void saveAccount();
+}
